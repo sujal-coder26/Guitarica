032e5934a1629c997ce99d0af21d9c4534ca6333
Sujal-coder24 <sujalgautam26@gmail.com>
2022-04-08 06:57:10 +0545

    Image upload made for user profile and did some minor changes

--- src/main/java/com/web/guitarapp/controller/HomeController.java
+++ src/main/java/com/web/guitarapp/controller/HomeController.java
@@ -20,13 +20,10 @@ import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.view.RedirectView;
 
-import javax.persistence.EntityManager;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
-import java.io.IOException;
 import java.security.Principal;
 import java.util.Optional;
 
@@ -35,9 +32,9 @@ import static com.web.guitarapp.util.Constants.*;
 @Controller
 @Slf4j
 @RequiredArgsConstructor
-public class HomeController {
+public class HomeController { //making controller
 
-  private final UserService userService;
+  private final UserService userService; //
 
   private final TrackerService trackerService;
 
@@ -117,21 +114,119 @@ public class HomeController {
     String username = principal.getName();
     System.out.println("Username: " + username);
     User user = userRepository.getUserByUserName(username);
+    Course course = trackerRepository.findByUserIdAndStatus(user, "ACTIVE").get().getCourseId();
+    System.out.println(course.getWeightage());
     model.addAttribute("user", user);
+    model.addAttribute("course", course);
     return "dashboard";
   }
+
+  @RequestMapping("/nextLevel")
+  public String insertData(Principal principal){
+    String username = principal.getName();
+    System.out.println(username);
+    User user = userRepository.getUserByUserName(username);
+    if (trackerRepository.findByUserIdAndStatus(user, "ACTIVE").isPresent()) {
+      trackerRepository.findByUserIdAndStatus(user, "ACTIVE").get().setStatus("COMPLETED");
+    }
+    Course course = new Course(1,"Beginner",1);
+    Tracker tracker = new Tracker(course, user, trackerRepository.findByUserIdAndStatus(user,"ACTIVE").get().getSubLevel() + 1, "ACTIVE");
+    trackerRepository.save(tracker);
+
+    return "redirect:/beginnerLevel";
+  }
+
+  @RequestMapping("/nextLevelInter")
+  public String insertDataInter(Principal principal){
+    String username = principal.getName();
+    User user = userRepository.getUserByUserName(username);
+    if (trackerRepository.findByUserIdAndStatus(user, "ACTIVE").isPresent()) {
+      trackerRepository.findByUserIdAndStatus(user, "ACTIVE").get().setStatus("COMPLETED");
+    }
+    Course course = new Course(2,"Intermediate",2);
+    Tracker tracker = new Tracker(course, user, trackerRepository.findByUserIdAndStatus(user,"ACTIVE").get().getSubLevel() + 1, "ACTIVE");
+    trackerRepository.save(tracker);
+
+    return "redirect:/intermediateLevel";
+  }
+
+  @RequestMapping("/nextLevelAdvance")
+  public String insertDataAdvance(Principal principal){
+    String username = principal.getName();
+    User user = userRepository.getUserByUserName(username);
+    if (trackerRepository.findByUserIdAndStatus(user, "ACTIVE").isPresent()) {
+      trackerRepository.findByUserIdAndStatus(user, "ACTIVE").get().setStatus("COMPLETED");
+    }
+    Course course = new Course(3,"Advance",3);
+    Tracker tracker = new Tracker(course, user, trackerRepository.findByUserIdAndStatus(user,"ACTIVE").get().getSubLevel() + 1, "ACTIVE");
+    trackerRepository.save(tracker);
+
+    return "redirect:/advanceLevel";
+  }
+
 
   @GetMapping("/beginnerLevel")
   public String beginnerLevel(Model model, Principal principal){
     String username = principal.getName();
     User user = userRepository.getUserByUserName(username);
     Optional<Tracker> optionalTracker = trackerRepository.findByUserIdAndStatus(user, "ACTIVE");
-
     int sub_level = optionalTracker.get().getSubLevel();
     System.out.println(sub_level);
     model.addAttribute("sublevel", sub_level);
     return "BeginnerLevel";
 
+  }
+  @GetMapping("/intermediateLevel")
+  public String intermediateLevel(Model model, Principal principal){
+    String usernamefind = principal.getName();
+    User userfind = userRepository.getUserByUserName(usernamefind);
+    Optional<Tracker> optionalTracker = trackerRepository.findByUserIdAndStatus(userfind, "ACTIVE");
+
+    int sub_level = optionalTracker.get().getSubLevel();
+    System.out.println(sub_level);
+    model.addAttribute("sub_level", sub_level);
+
+    return "IntermediateLevel";
+  }
+
+  @GetMapping("/advanceLevel")
+  public String advanceLevel(Model model, Principal principal){
+    String usernamefind = principal.getName();
+    User userfind = userRepository.getUserByUserName(usernamefind);
+    Optional<Tracker> optionalTracker = trackerRepository.findByUserIdAndStatus(userfind, "ACTIVE");
+
+    int sub_level = optionalTracker.get().getSubLevel();
+    System.out.println(sub_level);
+    model.addAttribute("sublevel", sub_level);
+
+    return "AdvanceLevel";
+  }
+
+  @GetMapping("/beginnerComplete")
+  public String beginnerFinish(Model model, Principal principal){
+    String username = principal.getName();
+    Course course = new Course(2,"Intermediate",2);
+    User user = userRepository.getUserByUserName(username);
+    if (trackerRepository.findByUserIdAndStatus(user, "ACTIVE").isPresent()) {
+      trackerRepository.findByUserIdAndStatus(user, "ACTIVE").get().setStatus("COMPLETED");
+    }
+    Tracker tracker = new Tracker(course, user, 6, "ACTIVE");
+    trackerService.save(tracker);
+
+    return "BeginnerLevel/beginnerComplete";
+  }
+  @GetMapping("/intermediateComplete")
+  public String intermediateFinish(Model model, Principal principal){
+    String username = principal.getName();
+    Course course = new Course(3,"Advance",3);
+    User user = userRepository.getUserByUserName(username);
+    if (trackerRepository.findByUserIdAndStatus(user, "ACTIVE").isPresent()) {
+      trackerRepository.findByUserIdAndStatus(user, "ACTIVE").get().setStatus("COMPLETED");
+    }
+    Tracker tracker = new Tracker(course, user, 11, "ACTIVE");
+    trackerService.save(tracker);
+
+    return "IntermediateLevel/intermediateComplete";
   }
 
   @GetMapping("/guitartuner")
@@ -139,18 +234,16 @@ public class HomeController {
     return "tuner";
   }
 
-  @GetMapping("/intermediateLevel")
-  public String intermediateLevel(){
-    return "IntermediateLevel";
-  }
-
-  @GetMapping("/advanceLevel")
-  public String advanceLevel(){
-    return "AdvanceLevel";
-  }
-
   @GetMapping("/completed")
-  public String completed(){
+  public String completed( Principal principal){
+    String username = principal.getName();
+    Course course = new Course(3,"Advance",3);
+    User user = userRepository.getUserByUserName(username);
+    if (trackerRepository.findByUserIdAndStatus(user, "ACTIVE").isPresent()) {
+      trackerRepository.findByUserIdAndStatus(user, "ACTIVE").get().setStatus("COMPLETED");
+    }
+    Tracker tracker = new Tracker(course, user, 16, "ACTIVE");
+    trackerService.save(tracker);
     return "certificate";
   }
 
